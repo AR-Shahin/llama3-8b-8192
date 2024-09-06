4f69debbec0072190d29e998c928914a345bfb5c
AR-Shahin <mdshahinmije96@gmail.com>
2024-09-06 12:31:31 +0600

    up

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
 import streamlit as st
 from langchain_groq import ChatGroq
 
-# Streamlit app title
-st.title("LLama3 Model")
+st.set_page_config(
+    page_title= "LLama 3.1-70b",
+    page_icon= '🤖'
+)
 
-# Define API key
+st.title("🤖 LLama3 Chatbot")
+
 api_key = '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'
 
-# Initialize ChatGroq model
 llm = ChatGroq(
     temperature=0,
     groq_api_key=api_key,
     model_name="llama-3.1-70b-versatile"
 )
 
-query = st.text_input("Write anything...")
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+    
 
+# Display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+        
+query = st.chat_input("Ask anything ...")
 if query:
-    st.write(f"User Input: {query}")
+    st.chat_message("user").markdown(query)
+    st.session_state.chat_history.append({"role": "user", "content": query})
 
     try:
+        # Send user's message to the LLM and get a response
         response = llm.invoke(query)
-        st.write(f"{response.content}")
+        assistant_response = response.content
+
+        # Save the assistant's response in chat history
+        st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+        # Display the assistant's response
+        with st.chat_message("assistant"):
+            st.markdown(assistant_response)
+
     except Exception as e:
         st.write(f"Error: {e}")
